a9b208108f9a0a3f11af4611c03d6edafa52a49c
Liuming <=>
2022-05-07 22:56:02 +0800

    memberjs

--- static/js/member.js
+++ static/js/member.js
@@ -19,78 +19,81 @@ comment =
     {
         "comname":"shop1",
         "comherf":"../home/",
-        "comcom" :"good",
-        "comtime":"2001/12/21"
+        "comcom" :"good1",
+        "comtime":"2001/12/21 00:00:00"
     },
     {
         "comname":"shop2",
         "comherf":"../home/",
-        "comcom" :"good",
-        "comtime":"2001/12/22"
+        "comcom" :"good2",
+        "comtime":"2001/12/22 00:00:00"
     },
     {
         "comname":"shop3",
         "comherf":"../home/",
-        "comcom" :"good",
-        "comtime":"2001/12/24"
+        "comcom" :"good3",
+        "comtime":"2001/12/24 00:00:00"
     },
     {
         "comname":"shop4",
         "comherf":"../home/",
-        "comcom" :"good",
-        "comtime":"2001/12/23"
+        "comcom" :"good4",
+        "comtime":"2001/12/23 00:00:00"
     },
     {
         "comname":"shop5",
         "comherf":"../home/",
-        "comcom" :"good",
-        "comtime":"2001/12/26"
+        "comcom" :"good5",
+        "comtime":"2001/12/26 00:00:00"
     },
     {
         "comname":"shop6",
         "comherf":"../home/",
-        "comcom" :"good",
-        "comtime":"2001/12/25"
+        "comcom" :"good6",
+        "comtime":"2001/12/25 00:00:00"
     },
     {
         "comname":"shop7",
         "comherf":"../home/",
-        "comcom" :"good",
-        "comtime":"2001/12/27"
+        "comcom" :"good7",
+        "comtime":"2001/12/27 01:20:00"
     },
     {
         "comname":"shop8",
         "comherf":"../home/",
-        "comcom" :"good",
-        "comtime":"2001/12/28"
+        "comcom" :"good8",
+        "comtime":"2001/12/27 01:00:00"
     },
     {
         "comname":"shop9",
         "comherf":"../home/",
-        "comcom" :"good",
-        "comtime":"2001/12/1"
+        "comcom" :"good9",
+        "comtime":"2001/12/1 00:00:00"
     },
     {
         "comname":"shop10",
         "comherf":"../home/",
-        "comcom" :"good",
-        "comtime":"2001/12/2"
+        "comcom" :"good10",
+        "comtime":"2001/12/2 00:00:00"
     },
     {
         "comname":"shop11",
         "comherf":"../home/",
-        "comcom" :"good",
-        "comtime":"2001/12/5"
+        "comcom" :"good11",
+        "comtime":"2001/12/5 00:00:00"
     },
     {
         "comname":"shop12",
         "comherf":"../home/",
-        "comcom" :"good",
-        "comtime":"2001/12/4"
+        "comcom" :"good12",
+        "comtime":"2001/12/4 00:00:00"
     }
 ]
 
 
+function sortdate(a, b){
+    return new Date(a.comtime).getTime() - new Date(b.comtime).getTime();
+}
 
 $(document).ready(function(){
     //here to start show love shop
@@ -105,13 +108,57 @@ $(document).ready(function(){
         }
     }
     //here to show comment record
-    var recomarr = document.getElementsByClassName("card-body p-4")
-    console.log(recomarr)
-    
+    comment.sort(sortdate).reverse()    //comment sort by datetime
+
+    if (comment.length == 0){
+        $('.container-fluid:nth(1)').html("");
+        $('.container-fluid:nth(1)').html(
+            `
+            <div class="container-fluid ">
+                <div class="row top">
+                    <div class="col-sm-2"></div>
+                    <div class="col-sm-8">
+                        <div class="card h-100 shadow border-0">
+                            <div class="card-body p-4">                       
+                                <h3>尚無任何評價紀錄</h3>
+                                <label></label>
+                            </div>  
+                        </div>  
+                    </div>
+                    <div class="col-sm-2"></div>
+                </div>
+                <div>
+                <nav aria-label="Page navigation">
+                    <ul class="pagination justify-content-center">
+                        <li class="page-item">
+                        <a class="page-link" href="#" aria-label="Previous">
+                            <span aria-hidden="true">«</span>
+                        </a>
+                        </li>
+                        <li class="page-item"><a class="page-link" href="#">1</a></li>
+                        <a class="page-link" href="#" aria-label="Next">
+                            <span aria-hidden="true">»</span>
+                        </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            </div>
+            `
+        );
+    }
+    else{
+        for (let i=0; i<10; i++){
+            ($(`.container-fluid:nth(1)`).find('h3')[i]).textContent    = `${comment[i]["comname"]}`
+            ($(`.container-fluid:nth(1)`).find('label')[i]).textContent = `${comment[i]["comcom"]}`
+        }
+    }
 })
+
+
 // namearr[i].textContent = hot[i]["name"]
 // imgarr[i].src = hot[i]["img"]
 // bodyarr[4*i].textContent = hot[i]["address"]
 // bodyarr[4*i+1].textContent = hot[i]["bestseller"]
 // bodyarr[4*i+2].textContent = hot[i]["comment0"]
-// bodyarr[4*i+3].textContent = hot[i]["comment1"]
+// bodyarr[4*i+3].textContent = hot[i]["comment1"]  
