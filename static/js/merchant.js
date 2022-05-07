6da2af00183c8ad16d5fe440c5f31f58e78dea2b
Liuming <=>
2022-05-07 23:51:08 +0800

    merchant

---
+++ static/js/merchant.js
@@ -0,0 +1,29 @@
+//merchant 優惠、商家資料、菜單、評價(前10筆)
+
+mdiscount = [
+    
+]
+
+minfor = {
+
+}
+
+mmenu = [
+    {
+
+    },
+    {
+
+    }
+]
+
+mcomment = [
+    {
+
+    }
+]
+
+$(document).ready(function(){
+
+   
+})
