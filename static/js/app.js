7ff60105babf226c9e7a95595037dc61608dd3c1
mark0613 <goldffd700@gmail.com>
2022-05-10 22:28:08 +0800

    update file

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,9 @@ function showAlertOnPage(text) {
     $("#warning").text(text);
     $("#warning").addClass("alert alert-danger")
 }
+
+$(document).ready(() => {
+    $('.datepicker').datepicker({
+        format: 'yyyy-mm-dd',
+    });
+});
