8213b2136331b8aa6a15422ccf445d41430751fa
mark0613 <goldffd700@gmail.com>
2022-05-15 22:46:13 +0800

    update file

--- php/getOrderById.php
+++ php/getOrderById.php
@@ -7,20 +7,16 @@ $response = [
 
 if (!empty($_GET['token'])) {
     $o_id = $_GET['token'];
-    $select = "SELECT * FROM orders WHERE o_id = $o_id";
+    $select = "SELECT * FROM orders WHERE o_id=$o_id";
     $result = Database::$connect->query($select);
     $i = 0;
     while ($row = $result->fetch_responseay(MYSQLI_ASSOC)){
         $response["status"] = "success";
         $response['data'][$i] = $row;
-        $i+=1;
+        $i += 1;
         
     }
 }
 
 header('Content-Type: application/json; charset=utf-8');
 echo json_encode($response, JSON_UNESCAPED_UNICODE);
-
-
-    
-?>
