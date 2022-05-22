9f926f6a6df122461ef5c25da07c05e7a803bb68
shacharumi <104343351+shacharumi@users.noreply.github.com>
2022-05-22 11:12:01 +0800

    Add files via upload

--- insert_loveShop.sql
+++ insert_loveShop.sql
@@ -1,3 +1,6 @@
-INSERT INTO `love_shop`(`c_id`, `m_id`) VALUES ('1','3');
-INSERT INTO `love_shop`(`c_id`, `m_id`) VALUES ('1','4');
-INSERT INTO `love_shop`(`c_id`, `m_id`) VALUES ('1','5');
+INSERT INTO `love_shop`(`l_id`,`c_id`, `m_id`) VALUES ('1','1','4');
+INSERT INTO `love_shop`(`l_id`,`c_id`, `m_id`) VALUES ('2','1','5');
+INSERT INTO `love_shop`(`l_id`,`c_id`, `m_id`) VALUES ('3','1','6');
+INSERT INTO `love_shop`(`l_id`,`c_id`, `m_id`) VALUES ('4','2','5');
+INSERT INTO `love_shop`(`l_id`,`c_id`, `m_id`) VALUES ('5','3','4');
+INSERT INTO `love_shop`(`l_id`,`c_id`, `m_id`) VALUES ('6','3','5');
